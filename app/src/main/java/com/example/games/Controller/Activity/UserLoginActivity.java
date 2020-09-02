e6841e67bf4e3ea38dd11b42a13edf33fa161bd7
FatemeAfshar20 <uttab4760@gmail.com>
2020-09-02 04:32:28 -0700

    Solved Problem

--- app/src/main/java/com/example/games/Controller/Activity/UserLoginActivity.java
+++ app/src/main/java/com/example/games/Controller/Activity/UserLoginActivity.java
@@ -1,23 +1,26 @@
 package com.example.games.Controller.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
-
+import com.example.games.Controller.Fragment.SingleFragment;
 import com.example.games.Controller.Fragment.UserLoginFragment;
 import com.example.games.R;
 
-public class UserLoginActivity extends AppCompatActivity {
-    FragmentManager mFragmentManager=getSupportFragmentManager();
-    UserLoginFragment mUserLoginFragment=new UserLoginFragment();
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_login);
+public class UserLoginActivity extends SingleFragment {
 
-        if(mUserLoginFragment!=null)
-            mFragmentManager.beginTransaction().add(R.id.user_login_Fragment,mUserLoginFragment).commit();
+    @Override
+    public Fragment getFragment() {
+        return new UserLoginFragment();
+    }
+
+    @Override
+    public int getContainer() {
+        return R.id.user_login_Fragment;
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_user_login;
     }
 
 }
--- app/src/main/java/com/example/games/Controller/Fragment/UserLoginFragment.java
+++ app/src/main/java/com/example/games/Controller/Fragment/UserLoginFragment.java
@@ -2,78 +2,83 @@ package com.example.games.Controller.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.example.games.Controller.Activity.GamesActivity;
-import com.example.games.Model.Player;
 import com.example.games.R;
-import com.example.games.SimpleViewModel;
+import com.varunest.sparkbutton.SparkButton;
 
 public class UserLoginFragment extends Fragment {
-    public static final String EXTRA_PLAYER_ONE_USERNAME = "com.example.games.Controller.Fragment.Player One User Name";
-    public static final String EXTRA_PLAYER_TWO_USERNAME = "com.example.games.Controller.Fragment.Player Two User Name";
-    public static final String BUNDLE_PLAYER_ONE_USERNAME = "Player One User Name";
-    public static final String BUNDLE_PLAYER_TWO_USERNAME = "Player Two User Name";
-    private Player mPlayerOne = new Player();
-    private Player mPlayerTwo = new Player();
-    String mNameOne,mNameTwo="hohoho";
+    public static final String EXTRA_PLAYER_ONE_USERNAME = "com.example.games.Controller.Fragment.User Name One";
+    public static final String EXTRA_PLAYER_TWO_USERNAME = "com.example.games.Controller.Fragment.User Name Two";
+    public static final String BUNDLE_USERNAME_ONE = "UserName One";
+    public static final String BUNDLE_USERNAME_TWO = "UserName Two";
+    SparkButton mBtnGo;
+    EditText mNameOneText,mNameTwoText;
 
-    private EditText mNamePlayerOne, mNamePlayerTwo;
-    private com.varunest.sparkbutton.SparkButton mBtnGo;
+    public UserLoginFragment(){
 
-    public UserLoginFragment() {
-        // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_user_login, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_user_login,container,false);
         findElem(view);
         setListener();
-        if(savedInstanceState!=null)
-            saveInstance(savedInstanceState);
-        //mViewModel= ViewModelProvider.of(getActivity()).get(SimpleViewModel.class);
+        saveInstance(savedInstanceState);
         return view;
     }
 
-
-    public void findElem(View view) {
-        mNamePlayerOne = view.findViewById(R.id.player_two);
-        mNamePlayerTwo = view.findViewById(R.id.player_one);
-        mBtnGo = view.findViewById(R.id.btn_go);
+    private void findElem(View view){
+        mBtnGo=view.findViewById(R.id.btn_go);
+        mNameOneText=view.findViewById(R.id.player_one);
+        mNameTwoText=view.findViewById(R.id.player_two);
     }
 
-    public void setListener() {
+    private void setListener(){
         mBtnGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkPlayer(mNamePlayerOne.getText().toString()) &&
-                        checkPlayer(mNamePlayerTwo.getText().toString())) {
-                    setPlayers(mPlayerOne, mNamePlayerOne.getText().toString());
-                    setPlayers(mPlayerTwo, mNamePlayerTwo.getText().toString());
-                    Intent intent = new Intent(getActivity(), GamesActivity.class);
-                    intent.putExtra(EXTRA_PLAYER_ONE_USERNAME, mPlayerOne.getUserName());
-                    intent.putExtra(EXTRA_PLAYER_TWO_USERNAME, mPlayerTwo.getUserName());
-                    startActivity(intent);
-                } else
+                if(checkPlayer(mNameOneText.getText().toString()) &&
+                        checkPlayer(mNameTwoText.getText().toString())){
+
+                        Intent intent = new Intent(getActivity(), GamesActivity.class);
+                        intent.putExtra(EXTRA_PLAYER_ONE_USERNAME, mNameOneText.getText().toString());
+                        intent.putExtra(EXTRA_PLAYER_TWO_USERNAME, mNameTwoText.getText().toString());
+                        startActivity(intent);
+                    }else
                     returnToast(R.string.toast_wrong);
             }
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        getActivity();
+        outState.getString(BUNDLE_USERNAME_ONE,mNameOneText.getText().toString());
+        outState.getString(BUNDLE_USERNAME_TWO,mNameTwoText.getText().toString());
+    }
+
+    public void saveInstance(Bundle bundle){
+        if(bundle!=null){
+            mNameOneText.setText(bundle.getString(BUNDLE_USERNAME_ONE));
+            mNameTwoText.setText(bundle.getString(BUNDLE_USERNAME_TWO));
+        }
     }
 
     private boolean checkPlayer(String str) {
@@ -82,27 +87,6 @@ public class UserLoginFragment extends Fragment {
         }
         returnToast(R.string.toast_username_players);
         return false;
-    }
-
-    private void setPlayers(Player player, String userName) {
-        player.setUserName(userName);
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        getActivity();
-        outState.putString(BUNDLE_PLAYER_ONE_USERNAME,mNamePlayerOne.getText().toString());
-        outState.putString(BUNDLE_PLAYER_TWO_USERNAME,mNamePlayerTwo.getText().toString());
-    }
-
-    public void saveInstance(Bundle bundle){
-        if(bundle!=null){
-            mNameOne=bundle.getString(BUNDLE_PLAYER_ONE_USERNAME);
-            mNamePlayerOne.setText(mNameOne);
-            mNameTwo=bundle.getString(BUNDLE_PLAYER_TWO_USERNAME);
-            mNamePlayerTwo.setText(mNameTwo);
-        }
     }
 
     private void returnToast(int strId) {
